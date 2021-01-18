63eed61a5fdc0a4c0407883e04efc8878a2a1561
utkarshverma09 <uterma59@gmail.com>
2021-01-18 19:05:52 +0530

    Adding files to repo

---
+++ plot2.R
@@ -0,0 +1,6 @@
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+subdata<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
+datetime<-strptime(paste(subdata$Date,subdata$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+png("plot2.png",width=480,height=480)
+plot(datetime,as.numeric(subdata$Global_active_power),type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
